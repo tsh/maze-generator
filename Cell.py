8ed9895b88da90db2f6c4a1de51357fcd26b4732
tsh <dr.tallin@gmail.com>
2017-08-13 21:40:40 +0300

    cell initial

--- Cell.py
+++ Cell.py
@@ -1,8 +1,42 @@
 class Cell:
-    def __init__(self):
-	    self.row = 0
-		self.column = 0
-		self.north = 0
-		self.south = 0
-		self.east = 0
-		self.west = 0
+    def __init__(self, row, col):
+        self.row = row
+        self.column = col
+        self.north = None
+        self.south = None
+        self.east = None
+        self.west = None
+        self.links = {
+            'north': 0,
+            'south': 0,
+            'east': 0,
+            'west': 0
+        }
+
+    def link(self, cell, bidi=True):
+        self.links[cell] = True
+        if bidi:
+            self.link(self, False)
+
+    def unlink(self, cell, bidi=True):
+        del self.links[cell]
+        if bidi:
+            self.unlink(self, False)
+
+    def links(self):
+        return self.links.keys()
+
+    def is_linked(self, cell):
+        return cell in self.links
+
+    def neighbors(self):
+        neib = []
+        if self.north:
+            neib.append(self.north)
+        if self.south:
+            neib.append(self.south)
+        if self.east:
+            neib.append(self.east)
+        if self.west:
+            neib.append(self.west)
+        return neib
